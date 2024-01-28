633db1d8e6d5b271e865a7961ca4b1e87d5ee092
Marcelo Diaz <adm_az_mdiaz@eaholding.onmicrosoft.com>
2024-01-28 02:15:09 -0300

    Segunda entrega

--- ProductManager.js
+++ ProductManager.js
@@ -1,40 +1,83 @@
+import fs from 'fs';
+
 
 class ProductManager {
-  
+    path = './ejemplo.json';
+
     constructor() {
         this.products = [];
+
     }
 
-    addProduct(product)  {
-        if(!product.title || !product.description || !product.price || !product.thumbnail || !product.stock || !product.code) {
+
+    addProduct(product) {
+        if (!product.title || !product.description || !product.price || !product.thumbnail || !product.stock || !product.code) {
             console.error("Faltan datos");
         } else if (this.products.some(prod => prod.code === product.code)) {
             console.error("El producto ya existe");
         } else {
-            console.log("Producto agregado");
+            console.log("Producto agregados");
             product.id = this.products.length + 1;
             this.products.push(product);
+            fs.writeFileSync(this.path, JSON.stringify(this.products));
         }
-    
-        
     }
 
+
     getProducts() {
-        return this.products;
+        let contenido = fs.readFileSync(this.path, 'utf8');
+        console.log("getProducts:",JSON.parse(contenido));
+
     }
 
     getProductById(id) {
-        const product = this.products.find(prod => prod.id === id);
-        if (product) {
-            return product;
+        let contenido = fs.readFileSync(this.path, 'utf8');
+        let JsonContenido = JSON.parse(contenido);
+        if (JsonContenido.find(prod => prod.id === id) === undefined) {
+            console.error("El producto no existe");
         } else {
-           return "El producto no existe";
+            console.log(JsonContenido.find(prod => prod.id === id));
+        }
+
+    }
+
+    putProductById(id, product) {
+        let contenido = fs.readFileSync(this.path, 'utf8');
+        let JsonContenido = JSON.parse(contenido);
+        let index = JsonContenido.findIndex(prod => prod.id === id);
+        if (JsonContenido[index] === undefined) {
+            console.error("El producto no existe");
+        } else {
+            let params = Object.keys(product);
+            params.forEach(param => {
+                JsonContenido[index][param] = product[param];
+            });
+            JsonContenido[index].id = id;
+            fs.writeFileSync(this.path, JSON.stringify(JsonContenido));
+            console.log("Producto actualizado", JsonContenido[index])
         }
     }
+
+    deleteProductById(id) {
+        let contenido = fs.readFileSync(this.path, 'utf8');
+        let JsonContenido = JSON.parse(contenido);
+        let index = JsonContenido.findIndex(prod => prod.id === id);
+        if (JsonContenido[index] === undefined) {
+            console.error("El producto no existe");
+        } else {
+            JsonContenido.splice(index, 1);
+            fs.writeFileSync(this.path, JSON.stringify(JsonContenido));
+            console.log("Producto eliminado");
+        }
+    }
+
+
 }
 
 
 const products = new ProductManager();
+
+products.getProducts();
 products.addProduct({
     title: "producto 1",
     description: "producto 1",
@@ -52,14 +95,26 @@ products.addProduct({
     code: "abc1234",
 });
 products.addProduct({
+    title: "producto 3",
+    description: "producto 3",
+    price: 300,
+    thumbnail: "https",
+    stock: 300,
+    code: "abc12345",
+});
+products.addProduct({
     title: "producto 1",
     description: "producto 1",
     price: 100,
     thumbnail: "https",
     stock: 10,
-    code: "abc123",
+    ode: "abc123",
 });
 products.addProduct({name: "Producto 2", price: 200});
-console.log(products.getProducts());
-console.log(products.getProductById(2));
-console.log(products.getProductById(5));
+products.getProducts();
+products.getProductById(2)
+products.getProductById(4)
+products.putProductById(2, {code: "abc1234", title: "producto 2"});
+products.putProductById(6, {code: "abc1234", title: "producto 2"});
+products.deleteProductById(5);
+products.deleteProductById(1);
